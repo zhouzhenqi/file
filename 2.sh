633059e3832a64bfe3a26cbcde02e88babf73b04
zhouzhenqi <823464517@qq.com>
2017-09-05 00:44:14 +0800

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -7,10 +7,10 @@ mv cloudtorrent/ /etc/
 cd /etc/cloudtorrent/
 chmod 777 cloud-torrent runct.sh
 sh runct.sh &
-#sleep 5
-#wget https://raw.githubusercontent.com/kuoruan/shell-scripts/master/ovz-bbr/ovz-bbr-installer.sh
-#chmod +x ovz-bbr-installer.sh
-#./ovz-bbr-installer.sh
+sleep 5
+wget https://raw.githubusercontent.com/kuoruan/shell-scripts/master/ovz-bbr/ovz-bbr-installer.sh
+chmod +x ovz-bbr-installer.sh
+./ovz-bbr-installer.sh
 #sleep 5
 #apt-get update && apt-get install -y zip unzip
 #wget --no-check-certificate -O backuptoqiniu.zip https://softs.fun/Website/backuptoqiniu.zip
