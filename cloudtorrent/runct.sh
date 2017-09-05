61fb1ee44259df89006af20490fffe326c4f4200
zhouzhenqi <823464517@qq.com>
2017-09-06 03:20:16 +0800

    file

--- cloudtorrent/runct.sh
+++ cloudtorrent/runct.sh
@@ -4,8 +4,7 @@ while true
 do
     procnum=` ps -ef|grep "cloud-torrent"|grep -v grep|wc -l`
    if [ $procnum -eq 0 ]; then
-       chmod 777 /etc/cloudtorrent/cloud-torrent
-       /etc/cloudtorrent/cloud-torrent -p 8888 -l  >> /dev/null 2>&1 &
+       /etc/cloudtorrent/cloud-torrent -p 80 >> /dev/null 2>&1 &
    fi
    sleep 30
 done
