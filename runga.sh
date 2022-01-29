a412d720dc5de787ab485ef7e68a3189da180149
zhouzhenqi <823464517@qq.com>
2022-01-29 21:52:27 +0800

    Update runga.sh

--- runga.sh
+++ runga.sh
@@ -8,14 +8,16 @@ unrar x github-ct.rar
 mv cloudtorrent/ /etc/
 cd /etc/cloudtorrent/
 chmod +x *
-./runct.sh &
+
 name=$(cat /etc/hostname)
 pport=$(echo $name | awk -F- '{print $3}')
 sed -i "s/http-80/"$name"-80/g" /etc/cloudtorrent/frpc.ini
 sed -i "s/tcp-5007/"$name"-5007/g" /etc/cloudtorrent/frpc.ini
 sed -i "s/udp-5007/"$name"-u5007/g" /etc/cloudtorrent/frpc.ini
 sed -i "s/6080/6"$pport"/g" /etc/cloudtorrent/frpc.ini
-sed -i "s/remote_port = 5007/remote_port = 6"$pport"/g" /etc/cloudtorrent/frpc.ini
+sed -i "s/5007/6"$pport"/g" /etc/cloudtorrent/frpc.ini
+sed -i "s/5007/6"$pport"/g" /etc/cloudtorrent/cloud-torrent.yaml
+./runct.sh &
 /etc/cloudtorrent/frpc -c /etc/cloudtorrent/frpc.ini &
 sleep 2
 apt -y autoremove docker docker-ce docker-engine  docker.io  containerd runc
