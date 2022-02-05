2912d095668db37b0df4a3d80251ab4e60d98c85
zhouzhenqi <823464517@qq.com>
2022-02-05 21:25:51 +0800

    Update runga2.sh

--- runga2.sh
+++ runga2.sh
@@ -14,7 +14,7 @@ b=$(($RANDOM%10))
 s=$(($RANDOM%10))
 g=$(($RANDOM%10))
 pport="$b$s$g"
-sed -i "s/http-80/"6$pport"-80/g" /etc/cloudtorrent/frpc.ini
+sed -i "s/http-80/6"$pport"-80/g" /etc/cloudtorrent/frpc.ini
 sed -i "s/tcp-5007/"$name"-5007/g" /etc/cloudtorrent/frpc.ini
 sed -i "s/udp-5007/"$name"-u5007/g" /etc/cloudtorrent/frpc.ini
 sed -i "s/6080/6"$pport"/g" /etc/cloudtorrent/frpc.ini
