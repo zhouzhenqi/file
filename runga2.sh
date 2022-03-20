2c132576466ea00bad96ab1b22996ce442a6c7b1
zhouzhenqi <823464517@qq.com>
2022-03-20 16:05:38 +0800

    Update runga2.sh

--- runga2.sh
+++ runga2.sh
@@ -1,6 +1,5 @@
 cd /root
 mkdir -p /sys/fs/cgroup/downloads
-yum clean all
 curl -O https://raw.githubusercontent.com/zhouzhenqi/file/master/ct2.tar.xz
 sleep 1
 xz -z -d ct2.tar.xz
@@ -23,5 +22,15 @@ sed -i "s/5007/5"$pport"/g" /etc/cloudtorrent/cloud-torrent.yaml
 sed -i 's;/etc/cloudtorrent/downloads;/sys/fs/cgroup/downloads;g' /etc/cloudtorrent/cloud-torrent.yaml
 ./runct.sh &
 /etc/cloudtorrent/frpc -c /etc/cloudtorrent/frpc.ini &
-
+yum-config-manager --disable kubernetes
+yum install passwd openssl openssh-server -y
+ssh-keygen -q -t rsa -b 2048 -f /etc/ssh/ssh_host_rsa_key -N ''
+ssh-keygen -q -t ecdsa -f /etc/ssh/ssh_host_ecdsa_key -N ''
+ssh-keygen -t dsa -f /etc/ssh/ssh_host_ed25519_key -N ''
+sed -i "s/#UsePrivilegeSeparation.*/UsePrivilegeSeparation no/g" /etc/ssh/sshd_config
+sed -i "s/UsePAM.*/UsePAM no/g" /etc/ssh/sshd_config
+echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+echo 123456 | passwd --stdin root
+/usr/sbin/sshd -D &
+yum clean all
 
