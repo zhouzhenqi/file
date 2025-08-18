1d2607de5352bbdef56d680559f35bf95e8d38e2
zhouzhenqi <823464517@qq.com>
2025-08-18 15:08:05 +0800

    Update runxr.sh

--- runxr.sh
+++ runxr.sh
@@ -6,13 +6,11 @@ sleep 1
 xz -z -d web.tar.xz
 tar -xvf web.tar
 chmod +x *
-myv6=$1
-sed -i "s/myv6/"$myv6"/g" ./config.json
 ./web.js -c ./config.json &
 cturl_S='aHR0cHM6Ly9naXRodWIuY29tL2Nsb3VkZmxhcmUvY2xvdWRmbGFyZWQvcmVsZWFzZXMvbGF0ZXN0L2Rvd25sb2FkL2Nsb3VkZmxhcmVkLWxpbnV4LWFtZDY0Cg=='
 cturl=$(printf "%s" $cturl_S | base64 -d) 
 curl -o cf -L $cturl
 chmod +x cf
-ARGO_ARGS_S='IC0tcHJvdG9jb2wgcXVpYyAtLWVkZ2UtaXAtdmVyc2lvbiA0IC0tdXJsIGh0dHA6Ly9sb2NhbGhvc3Q6MzAwMiB0dW5uZWwgcnVuIC0tdG9rZW4gZXlKaElqb2lPVE01WmpObVkyVXpNekl5T0dWall6Tm1Zak0wT0RsaVlURmlNV1V3TVRZaUxDSjBJam9pTmpNME9EazROamN0WmpBMk1TMDBORFpqTFRrNU1qVXRaVGM0WkRJNU16ZGtaREJoSWl3aWN5STZJbGxVUlhwTlZGWnFXa1JuZEZwWFRtcE9VekF3VGtkVk5FeFhSbTFOTWtsMFRUSkthRTF0U1RSYVJHZDRUV3BCTkNKOQ=='
+ARGO_ARGS_S='LS1wcm90b2NvbCBodHRwMiAtLWVkZ2UtaXAtdmVyc2lvbiA0IHR1bm5lbCBydW4gLS10b2tlbiBleUpoSWpvaU9UTTVaak5tWTJVek16SXlPR1ZqWXpObVlqTTBPRGxpWVRGaU1XVXdNVFlpTENKMElqb2lZek0xTWpneE9UY3ROR1kzTkMwMFpUTTBMV0k0TjJZdE1ETTRNR0ptWkRobU4yUXhJaXdpY3lJNklrMVhWWHBaYlZWNldtcFZkRTFxUVRCYWFUQXdUV3BKZDB4VVozbFBWR2QwVFVSWmVrMTZhR3BPYlZwcFRsUmFiQ0o5Cg=='
 ARGO_ARGS=$(printf "%s" $ARGO_ARGS_S | base64 -d) 
 nohup ./cf $ARGO_ARGS >/dev/null 2>&1 &
